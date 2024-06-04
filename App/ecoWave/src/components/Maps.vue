af641e52cb98f83cf86945ddffcb4b6880d9c48a
termimi <123395118+termimi@users.noreply.github.com>
2024-06-04 11:04:08 +0200

    maps oke

--- App/ecoWave/src/components/Maps.vue
+++ App/ecoWave/src/components/Maps.vue
@@ -1,7 +1,37 @@
-<script setup>
-
+<script>
+import { onMounted,ref } from 'vue';
+export default { 
+  name: 'Map', 
+  setup() { 
+    const mapRef = ref(null); 
+    onMounted(() => { 
+       
+            const tt = window.tt; 
+            var map = tt.map({ 
+            key: 'R7dnyFDjCXpftwFLBGDFaklxWOOpPPsG', 
+            container: mapRef.value, 
+            style: 'tomtom://vector/1/basic-main', 
+        }); 
+        map.addControl(new tt.FullscreenControl()); 
+        map.addControl(new tt.NavigationControl());  
+        AddMarker(map);
+    }) 
+    return { 
+      mapRef, 
+    }; 
+  } 
+} 
 </script>
+<style> 
+.map { 
+    height: 50vh; 
+    width: 50vw; 
+} 
+</style> 
 
 <template>
-    
+    <h1>Maps</h1>
+    <div class="map" ref="mapRef">
+
+    </div>
 </template>
