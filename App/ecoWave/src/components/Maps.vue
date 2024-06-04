0422d3cfc9cd7fb299e4a2b0ad35b8ab50857cd6
termimi <123395118+termimi@users.noreply.github.com>
2024-06-04 11:25:28 +0200

    geoLoca +- finis

--- App/ecoWave/src/components/Maps.vue
+++ App/ecoWave/src/components/Maps.vue
@@ -1,5 +1,23 @@
 <script>
 import { onMounted,ref } from 'vue';
+const userLongitude = ref(null);
+const userLatitude = ref(null);
+const getLocationOfUser = ()=>{
+    navigator.geolocation.getCurrentPosition((position) => {
+        userLatitude = position.coords.latitude;
+        userLongitude = position.coords.longitude;
+    })
+}
+const AddMarker =(map)=>{
+    const tt = window.tt; 
+    var location = [-121.91595, 37.36729]; 
+    var popupOffset = 25; 
+ 
+    var marker = new tt.Marker().setLngLat(location).addTo(map); 
+    var popup = new tt.Popup({ offset: popupOffset }); 
+    reverseGeocoding(marker, popup); 
+    marker.setPopup(popup).togglePopup(); 
+}
 export default { 
   name: 'Map', 
   setup() { 
@@ -19,7 +37,7 @@ export default {
     return { 
       mapRef, 
     }; 
-  } 
+  }, 
 } 
 </script>
 <style> 
