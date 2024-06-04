69c4ffd542ece58e9d4260ca928cc29afc2c7703
termimi <123395118+termimi@users.noreply.github.com>
2024-06-04 16:33:29 +0200

    geoLoc refactored

--- App/ecoWave/src/components/Maps.vue
+++ App/ecoWave/src/components/Maps.vue
@@ -2,40 +2,56 @@
 import { onMounted,ref } from 'vue';
 let userLongitude = ref(null);
 let userLatitude = ref(null);
-let latitude =ref(null);
-const getLocationOfUser = async ()=>{
-    
+let errorLoadingMap = ref(null);
+const getUserLocation = () =>{
+    return new Promise((resolve,reject) => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            userLatitude.value = position.coords.latitude;
+            userLongitude.value = position.coords.longitude
+            resolve();
+        },
+        (error) => {
+            reject(error)
+        },
+        {
+            enableHighAccuracy: true, // Demander une haute précision
+            timeout: 10000,           // Temps maximum pour obtenir la position (en millisecondes)
+            maximumAge: 0             // Empêcher la mise en cache de la position
+        });
+    }) 
 }
 const AddMarker =(map,latitude,longitude)=>{
     const tt = window.tt; 
     var location = [longitude, latitude]; 
     var popupOffset = 25; 
- 
     var marker = new tt.Marker().setLngLat(location).addTo(map); 
     var popup = new tt.Popup({ offset: popupOffset }); 
-    reverseGeocoding(marker, popup); 
+    //reverseGeocoding(marker, popup); 
     marker.setPopup(popup).togglePopup(); 
 }
 export default { 
   name: 'Map', 
   setup() { 
     const mapRef = ref(null); 
-    onMounted(() => { 
-       
+    onMounted(async () => { 
+        try{
+            await getUserLocation()
             const tt = window.tt; 
             var map = tt.map({ 
-            key: 'R7dnyFDjCXpftwFLBGDFaklxWOOpPPsG', 
-            container: mapRef.value, 
-            style: 'tomtom://vector/1/basic-main', 
-        }); 
-        map.addControl(new tt.FullscreenControl()); 
-        map.addControl(new tt.NavigationControl());
-        console.log("lattt :" + userLatitude.value);
-        navigator.geolocation.getCurrentPosition((position) => {
-            userLatitude.value = position.coords.latitude;
-            userLongitude.value = position.coords.longitude;
+                key: 'R7dnyFDjCXpftwFLBGDFaklxWOOpPPsG', 
+                container: mapRef.value, 
+                style: 'tomtom://vector/1/basic-main',
+                zoom:15,
+                center:[userLongitude.value,userLatitude.value]
+            }); 
+            map.addControl(new tt.FullscreenControl()); 
+            map.addControl(new tt.NavigationControl());
+            console.log(userLatitude.value)
             AddMarker(map,userLatitude.value,userLongitude.value);
-        })
+        }
+        catch{
+            errorLoadingMap = true
+        }
     }) 
     return { 
       mapRef, 
@@ -53,6 +69,7 @@ export default {
 <template>
     <h1>Maps</h1>
     <div class="map" ref="mapRef">
-
+        
     </div>
+    
 </template>
