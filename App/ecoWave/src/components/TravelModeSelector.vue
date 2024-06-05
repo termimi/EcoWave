dc75dbe9374728aea4d6a1602db89cd03b4f0934
EvinPara <96207677+Evin06@users.noreply.github.com>
2024-06-05 11:25:38 +0200

    test

---
+++ App/ecoWave/src/components/TravelModeSelector.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="tt-overlay-panel" style="width: 320px;">
+    <div class="tt-form-label">
+      Travel mode
+      <select :value="travelMode" @change="handleTravelModeChange" class="tt-select">
+        <option value="pedestrian">Pedestrian</option>
+        <option value="car">Car</option>
+        <option value="truck">Truck</option>
+        <option value="bicycle">Bicycle</option>
+        <option value="bus">Bus</option>
+        <option value="van">Van</option>
+        <option value="motorcycle">Motorcycle</option>
+        <option value="taxi">Taxi</option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TravelModeSelector',
+  props: {
+    travelMode: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    handleTravelModeChange(event) {
+      this.$emit('update:travelMode', event.target.value);
+      this.$emit('travel-mode-changed', event.target.value);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.tt-overlay-panel {
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  background: white;
+  padding: 10px;
+  z-index: 1;
+}
+.tt-form-label {
+  margin-bottom: 10px;
+}
+.tt-select {
+  width: 100%;
+  padding: 5px;
+}
+</style>
