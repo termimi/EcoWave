947863d6e39a5ee74c3ecf0151266682a490d8f2
EvinPara <96207677+Evin06@users.noreply.github.com>
2024-06-05 22:43:09 +0200

    Update App.vue

--- App/ecoWave/src/App.vue
+++ App/ecoWave/src/App.vue
@@ -1,15 +1,14 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import Maps  from './components/Maps.vue';
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/maps">Map</RouterLink>
