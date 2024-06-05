c3b2c05c3b803d65b515e2abff2c2b298d2c3726
EvinPara <96207677+Evin06@users.noreply.github.com>
2024-06-05 14:38:02 +0200

    update last version from main

--- App/ecoWave/src/components/Maps.vue
+++ App/ecoWave/src/components/Maps.vue
@@ -1,72 +1,73 @@
 <script>
-import { onMounted,ref } from 'vue';
-let userLongitude = ref(null);
-let userLatitude = ref(null);
-let errorLoadingMap = ref(null);
-import { createSearchBox} from './features/routing.vue';
-const getUserLocation = () =>{
-    return new Promise((resolve,reject) => {
-        navigator.geolocation.getCurrentPosition((position) => {
-            userLatitude.value = position.coords.latitude;
-            userLongitude.value = position.coords.longitude
-            resolve();
-        },
-        (error) => {
-            reject(error)
-        },
-        {
-            enableHighAccuracy: true, // Demander une haute précision
-            timeout: 10000,           // Temps maximum pour obtenir la position (en millisecondes)
-            maximumAge: 0             // Empêcher la mise en cache de la position
-        });
-    })
+import { onMounted, ref } from 'vue'
+let userLongitude = ref(null)
+let userLatitude = ref(null)
+let errorLoadingMap = ref(null)
+import { createSearchBox } from './features/routing.vue'
+const getUserLocation = () => {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        userLatitude.value = position.coords.latitude
+        userLongitude.value = position.coords.longitude
+        resolve()
+      },
+      (error) => {
+        reject(error)
+      },
+      {
+        enableHighAccuracy: true, // Demander une haute précision
+        timeout: 10000, // Temps maximum pour obtenir la position (en millisecondes)
+        maximumAge: 0 // Empêcher la mise en cache de la position
+      }
+    )
+  })
 }
-const AddMarker =(map,latitude,longitude)=>{
-    const tt = window.tt;
-    var location = [longitude, latitude];
-    var popupOffset = 25;
-    var marker = new tt.Marker().setLngLat(location).addTo(map);
-    var popup = new tt.Popup({ offset: popupOffset });
-    //reverseGeocoding(marker, popup);
-    marker.setPopup(popup).togglePopup();
+const AddMarker = (map, latitude, longitude) => {
+  const tt = window.tt
+  var location = [longitude, latitude]
+  var popupOffset = 25
+  var marker = new tt.Marker().setLngLat(location).addTo(map)
+  var popup = new tt.Popup({ offset: popupOffset })
+  //reverseGeocoding(marker, popup);
+  marker.setPopup(popup).togglePopup()
 }
 export default {
   name: 'Map',
   setup() {
-    const mapRef = ref(null);
+    const mapRef = ref(null)
     onMounted(async () => {
-        try{
-            await getUserLocation()
-            const tt = window.tt;
-            var map = tt.map({
-                key: 'R7dnyFDjCXpftwFLBGDFaklxWOOpPPsG',
-                container: mapRef.value,
-                style: 'tomtom://vector/1/basic-main',
-                zoom:15,
-                center:[userLongitude.value,userLatitude.value]
-            });
-            map.addControl(new tt.FullscreenControl());
-            map.addControl(new tt.NavigationControl());
-            console.log(userLatitude.value)
-            AddMarker(map,userLatitude.value,userLongitude.value);
- 
-            createSearchBox('start',map);
-            createSearchBox('finish',map);
-        }
-        catch{
-            errorLoadingMap = true
-        }
+      try {
+        await getUserLocation()
+        const tt = window.tt
+        var map = tt.map({
+          key: 'R7dnyFDjCXpftwFLBGDFaklxWOOpPPsG',
+          container: mapRef.value,
+          style: 'tomtom://vector/1/basic-main',
+          zoom: 15,
+          center: [userLongitude.value, userLatitude.value]
+        })
+        map.addControl(new tt.FullscreenControl())
+        map.addControl(new tt.NavigationControl())
+        console.log(userLatitude.value)
+        AddMarker(map, userLatitude.value, userLongitude.value)
+
+        createSearchBox('start', map)
+        createSearchBox('finish', map)
+      } catch {
+        errorLoadingMap = true
+      }
     })
     return {
-      mapRef,
-    };
-  },
+      mapRef
+    }
+  }
 }
 </script>
 <style>
 .map {
-    height: 50vh;
-    width: 50vw;
+  height: 50vh;
+  width: 50vw;
 }
 .tt-overlay-panel {
   position: absolute;
@@ -87,25 +88,24 @@ export default {
   display: flex;
   height: 32px;
   justify-content: center;
-  transition: width 0.1s, height 0.1s;
+  transition:
+    width 0.1s,
+    height 0.1s;
   width: 32px;
 }
 </style>
- 
+
 <template>
-    <h1>Maps</h1>
-    <div class="map" ref="mapRef">
-   
-    </div>
-    <div id="foldable" class="tt-overlay-panel -left-top -medium js-foldable">
-      <form id="form">
-        <div id="startSearchBox" class="searchbox-container">
-          <div class="tt-icon tt-icon-size icon-spacing-right -start"></div>
-        </div>
-        <div id="finishSearchBox" class="searchbox-container">
-          <div class="tt-icon tt-icon-size icon-spacing-right -finish"></div>
-        </div>
-      </form>
-    </div>
-   
+  <h1>Maps</h1>
+  <div class="map" ref="mapRef"></div>
+  <div id="foldable" class="tt-overlay-panel -left-top -medium js-foldable">
+    <form id="form">
+      <div id="startSearchBox" class="searchbox-container">
+        <div class="tt-icon tt-icon-size icon-spacing-right -start"></div>
+      </div>
+      <div id="finishSearchBox" class="searchbox-container">
+        <div class="tt-icon tt-icon-size icon-spacing-right -finish"></div>
+      </div>
+    </form>
+  </div>
 </template>
--- App/ecoWave/src/components/features/routing.vue
+++ App/ecoWave/src/components/features/routing.vue
@@ -1,234 +1,152 @@
 <script>
-import { onMounted, ref } from 'vue';
- 
+import { onMounted, ref } from 'vue'
+
 export const state = {
   start: undefined,
   finish: undefined,
   marker: {
     start: undefined,
-    finish: undefined,
-  },
-};
-const getUserLocation = () => {
-  return new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        resolve({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude
-        });
-      },
-      (error) => {
-        reject(error);
-      },
-      {
-        enableHighAccuracy: true,
-        timeout: 10000,
-        maximumAge: 0
-      }
-    );
-  });
-};
- 
-const addMarker = (map, latitude, longitude) => {
-  const tt = window.tt;
-  const location = [longitude, latitude];
-  const marker = new tt.Marker().setLngLat(location).addTo(map);
-  const popup = new tt.Popup({ offset: 25 }).setText('You are here');
-  marker.setPopup(popup).togglePopup();
-};
- 
- 
-const drawMarker = (type) => {
-  const tt = window.tt;
-  if (state.marker[type]) {
-    state.marker[type].remove();
+    finish: undefined
   }
- 
-  const marker = document.createElement('div');
-  const innerElement = document.createElement('div');
- 
-  marker.className = 'route-marker';
-  innerElement.className = `icon tt-icon -white -${type}`;
-  marker.appendChild(innerElement);
- 
-  state.marker[type] = new tt.Marker({ element: marker })
-    .setLngLat(state[type])
-    .addTo(map);
- 
-  updateBounds();
-};
- 
-const updateBounds = () => {
-  const tt = window.tt;
-  const bounds = new tt.LngLatBounds();
- 
+}
+const drawMarker = (type, map) => {
+  const tt = window.tt
+  if (state.marker[type]) {
+    state.marker[type].remove()
+  }
+
+  const marker = document.createElement('div')
+  const innerElement = document.createElement('div')
+
+  marker.className = 'route-marker'
+  innerElement.className = `icon tt-icon -white -${type}`
+  marker.appendChild(innerElement)
+
+  state.marker[type] = new tt.Marker({ element: marker }).setLngLat(state[type]).addTo(map)
+
+  updateBounds(map)
+}
+
+const updateBounds = (map) => {
+  const tt = window.tt
+  const bounds = new tt.LngLatBounds()
+
   if (state.start) {
-    bounds.extend(tt.LngLat.convert(state.start));
+    bounds.extend(tt.LngLat.convert(state.start))
   }
   if (state.finish) {
-    bounds.extend(tt.LngLat.convert(state.finish));
+    bounds.extend(tt.LngLat.convert(state.finish))
   }
- 
+
   if (!bounds.isEmpty()) {
-    map.fitBounds(bounds, { duration: 0, padding: 50 });
+    map.fitBounds(bounds, { duration: 0, padding: 50 })
   }
-};
-const updateRoutesBounds = (coordinates) => {
-  const tt = window.tt;
-  const bounds = new tt.LngLatBounds();
- 
+}
+const updateRoutesBounds = (coordinates, map) => {
+  const tt = window.tt
+  const bounds = new tt.LngLatBounds()
+
   coordinates.forEach((point) => {
-    bounds.extend(tt.LngLat.convert(point));
-  });
- 
+    bounds.extend(tt.LngLat.convert(point))
+  })
+
   if (!bounds.isEmpty()) {
-    map.fitBounds(bounds, { duration: 0, padding: 50 });
+    map.fitBounds(bounds, { duration: 0, padding: 50 })
   }
-};
- 
-const calculateRoute = (map,state) => {
+}
+
+const calculateRoute = (map, state) => {
   try {
-    const tt = window.tt;
+    const tt = window.tt
     if (map.getLayer('route')) {
-      map.removeLayer('route');
-      map.removeSource('route');
+      map.removeLayer('route')
+      map.removeSource('route')
     }
- 
+
     if (!state.start || !state.finish) {
-      return;
+      return
     }
- 
-    const startPos = state.start.join(',');
-    const finalPos = state.finish.join(',');
- 
-    tt.services.calculateRoute({
-      key: 'R7dnyFDjCXpftwFLBGDFaklxWOOpPPsG',
-      traffic: false,
-      locations: `${startPos}:${finalPos}`,
-    })
+
+    const startPos = state.start.join(',')
+    const finalPos = state.finish.join(',')
+
+    tt.services
+      .calculateRoute({
+        key: 'R7dnyFDjCXpftwFLBGDFaklxWOOpPPsG',
+        traffic: false,
+        locations: `${startPos}:${finalPos}`
+      })
       .then((response) => {
-        const geojson = response.toGeoJson();
+        const geojson = response.toGeoJson()
         map.addLayer({
           id: 'route',
           type: 'line',
           source: {
             type: 'geojson',
-            data: geojson,
+            data: geojson
           },
           paint: {
             'line-color': '#2faaff',
-            'line-width': 8,
-          },
-        });
- 
-        const coordinates = geojson.features[0].geometry.coordinates;
-        updateRoutesBounds(coordinates);
+            'line-width': 8
+          }
+        })
+
+        const coordinates = geojson.features[0].geometry.coordinates
+        updateRoutesBounds(coordinates, map)
       })
       .catch((error) => {
-        console.error(error);
-      });
-  }
-  catch(error){
+        console.error(error)
+      })
+  } catch (error) {
     console.log(error)
   }
- 
-};
- 
- 
- 
+}
+
 export const onResultSelected = (result, type, map) => {
-  const pos = result.position;
-  state[type] = [pos.lng, pos.lat];
- 
-  drawMarker(type);
-  calculateRoute(map,state);
-};
- 
+  const pos = result.position
+  state[type] = [pos.lng, pos.lat]
+
+  drawMarker(type, map)
+  calculateRoute(map, state)
+}
+
 export const onResultCleared = (type, map) => {
-  state[type] = undefined;
- 
+  state[type] = undefined
+
   if (state.marker[type]) {
-    state.marker[type].remove();
-    state.marker[type] = undefined;
-    updateBounds();
+    state.marker[type].remove()
+    state.marker[type] = undefined
+    updateBounds()
   }
- 
-  calculateRoute(map,state);
-};
+
+  calculateRoute(map, state)
+}
 export const createSearchBox = (type, map) => {
-  const tt = window.tt;
+  const tt = window.tt
   try {
     const searchBox = new tt.plugins.SearchBox(tt.services, {
       showSearchButton: false,
       searchOptions: {
-        key: 'R7dnyFDjCXpftwFLBGDFaklxWOOpPPsG',
+        key: 'R7dnyFDjCXpftwFLBGDFaklxWOOpPPsG'
       },
       labels: {
-        placeholder: type === 'start' ? 'Start Location' : 'End Location',
-      },
-    });
- 
-    document.getElementById(`${type}SearchBox`).appendChild(searchBox.getSearchBoxHTML());
- 
+        placeholder: type === 'start' ? 'Start Location' : 'End Location'
+      }
+    })
+
+    document.getElementById(`${type}SearchBox`).appendChild(searchBox.getSearchBoxHTML())
+
     searchBox.on('tomtom.searchbox.resultselected', function (event) {
       if (event.data && event.data.result) {
-        onResultSelected(event.data.result, type, map);
+        onResultSelected(event.data.result, type, map)
       }
-    });
- 
+    })
+
     searchBox.on('tomtom.searchbox.resultscleared', function () {
-      onResultCleared(type, map);
-    });
-  }
-  catch (error) {
+      onResultCleared(type, map)
+    })
+  } catch (error) {
     console.log(error)
   }
- 
-};
- 
-export default {
-  name: 'Map',
-  setup() {
-    const mapRef = ref(null);
-    let map;
- 
- 
-    const initializeMap = async () => {
-      try {
-        const userLocation = await getUserLocation();
-        const tt = window.tt;
-        map = tt.map({
-          key: 'R7dnyFDjCXpftwFLBGDFaklxWOOpPPsG',
-          container: mapRef.value,
-          style: 'https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.25.1/mapstyles/basic_main.json',
-          center: [userLocation.longitude, userLocation.latitude],
-          zoom: 15
-        });
- 
-        map.addControl(new tt.FullscreenControl());
-        map.addControl(new tt.NavigationControl());
-        addMarker(map, userLocation.latitude, userLocation.longitude);
- 
-        createSearchBox('start');
-        createSearchBox('finish');
-      } catch (error) {
-        console.error('Error loading map:', error);
-      }
-    };
- 
-    onMounted(() => {
-      if (window.tt) {
-        initializeMap();
-      } else {
-        console.error('TomTom SDK not loaded');
-      }
-    });
- 
-    return {
-      mapRef,
-    };
-  },
-};
+}
 </script>
- 
